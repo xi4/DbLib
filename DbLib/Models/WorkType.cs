6dec06ceea26f5bf642b8cc2cdc6eb1188e0f72a
kirill <beelzebul.18@gmail.com>
2020-08-20 18:06:43 +0300

    Параметр скрырия

--- DbLib/Models/WorkType.cs
+++ DbLib/Models/WorkType.cs
@@ -11,6 +11,7 @@ namespace DbLib.Models
         public string txt_eng { get; set; }
         public bool hide { get; set; }
         public int ord { get; set; }
+        public int orded { get; set; }
         public ICollection<WorkListing>  workListings { get; set; }
     }
 }
