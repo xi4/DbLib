ba689498dcbd55502fa8975526dcb8c143b34345
kirill <beelzebul.18@gmail.com>
2020-08-20 17:38:34 +0300

    Английстик текст в тип работ

--- DbLib/Models/WorkType.cs
+++ DbLib/Models/WorkType.cs
@@ -8,6 +8,7 @@ namespace DbLib.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string txt { get; set; }
+        public string txt_eng { get; set; }
         public int ord { get; set; }
         public ICollection<WorkListing>  workListings { get; set; }
     }
